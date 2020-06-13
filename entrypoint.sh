23271295d780516416bf6c3909570a1a75aa39f6
Naoki Ikeguchi <root@siketyan.dev>
2020-06-14 01:48:18 +0900

    Use variables to determine directories or files

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,18 +6,26 @@ SSH_USER=${INPUT_SSH_USER}
 SSH_KEY=${INPUT_SSH_KEY}
 SOURCE_DIRECTORY=${GITHUB_WORKSPACE}
 
-echo "$SSH_KEY" > ./identity
-chmod 600 ./identity
+SSH_DIR="$HOME/.ssh"
+SSH_CONFIG="$SSH_DIR/config"
+SSH_IDENTITY="$SSH_DIR/identity"
+SSH_KNOWN_HOSTS="$SSH_DIR/known_hosts"
+SSH_COMMAND="ssh -F $SSH_CONFIG"
+RSYNC_COMMAND="rsync -a -v -e \"$SSH_COMMAND\""
 
-cat - << EOS > ./config
+echo "$SSH_KEY" > $SSH_IDENTITY
+chmod 600 $SSH_IDENTITY
+
+cat - << EOS > $SSH_CONFIG
 Host $SSH_HOST
     User $SSH_USER
     Port $SSH_PORT
-    IdentityFile ./identity
+    IdentityFile $SSH_IDENTITY
+    UserKnownHostsFile $SSH_KNOWN_HOSTS
     StrictHostKeyChecking no
 EOS
 
-ssh -F ./config $SSH_HOST mkdir -p "~/.cdep/repo"
-rsync -a -v -e "ssh -F ./config" /deploy.sh "$SSH_HOST:~/.cdep/"
-rsync -a -v -e "ssh -F ./config" --delete --exclude ".git" "$SOURCE_DIRECTORY/" "$SSH_HOST:~/.cdep/repo/"
-ssh -F ./config $SSH_HOST "~/.cdep/deploy.sh"
+$SSH_COMMAND $SSH_HOST mkdir -p "~/.cdep/repo"
+$RSYNC_COMMAND /deploy.sh "$SSH_HOST:~/.cdep/"
+$RSYNC_COMMAND --delete --exclude ".git" "$SOURCE_DIRECTORY/" "$SSH_HOST:~/.cdep/repo/"
+$SSH_COMMAND $SSH_HOST "~/.cdep/deploy.sh"
